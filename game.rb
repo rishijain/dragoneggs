0db0fba9b5bed22cd2842ad38d52a3ec73f643ad
Rishi Jain <rishi@joshsoftware.com>
2014-07-11 19:41:47 +0530

    added comments for better understanding.

--- game.rb
+++ game.rb
@@ -3,20 +3,27 @@ require 'gosu'
 EGGCOUNT = 100
 GRAVITY = 5
 BASKETCOUNT = 500
+RINGSPEED = 4
 
 class Game < Gosu::Window
 
   def initialize
     super 800, 600, false
     self.caption = 'Drop the f*ing egg.'
+    @bg = Gosu::Image.new self, 'cloud.jpg'
+    @logo = Gosu::Image.new self, 'logo.png'
+    
+    #initialize the eggs
     @eggs = []
     EGGCOUNT.times {|d| @eggs << Egg.new(self, 400*d + 400, 100)}
+    
+    #initialize the rings
     @rings = []
     BASKETCOUNT.times  {|d| @rings << Basket.new(self, 400*d + 600, 500 - 10*rand(20))}
+    
     @fall_count = 0
-    @bg = Gosu::Image.new self, 'cloud.jpg'
     @score = 0
-    @logo = Gosu::Image.new self, 'logo.png'
+    
     @score_message = Gosu::Image.new self, 'score_message.png'
     @score_value = Gosu::Font.new(self, Gosu::default_font_name, 35)
     @game_track = Gosu::Song.new(self, "gametrack.mp3")
@@ -34,24 +41,41 @@ class Game < Gosu::Window
   def update
     @game_track.play(true)
     current_egg = @eggs[@fall_count]
-    @eggs.each {|d| d.x = d.x - 4 if d.already_counted}
+    
+    #to move the egg when it falls on the basket
+    @eggs.each {|d| d.x = d.x - RINGSPEED if d.already_counted}
+
+    #for the current egg to keep falling
     current_egg.y = current_egg.y + GRAVITY if current_egg.free_fall 
+    
+    #when pressed space, current egg should fall
     if button_down?(Gosu::KbSpace)
       current_egg.free_fall = true
+    
+    #to move the curret egg left and right only when it is at its starting position 
     elsif button_down?(Gosu::KbLeft) && current_egg.free_fall != true
       current_egg.x = current_egg.x - 10 if current_egg.x > 100
     elsif button_down?(Gosu::KbRight) && current_egg.free_fall != true
       current_egg.x = current_egg.x + 10 if current_egg.x < 700
     end
 
+    #if current egg has fallen off the screen or is into the basket
     if free_fall_completed?(current_egg)
       @fall_count += 1 
+
+      #find the next egg in array which should be new current egg
       next_egg = @eggs[@eggs.index(current_egg) + 1]
+
+      #the new current egg should move -400 to come within the visible part of the screen
       next_egg.x = next_egg.x - (400 * @fall_count)
     end
 
     @rings.each do |d| 
-      d.x = d.x - 4
+
+      #the ring should always keep moving to left
+      d.x = d.x - RINGSPEED
+
+      #check the egg is fallen into basket only if current egg is not counted (i.e not in the basket already)
       if egg_into_basket(current_egg, d) && !current_egg.already_counted
         current_egg.already_counted = true
         current_egg.free_fall = false
